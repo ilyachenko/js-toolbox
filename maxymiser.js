2da579e22d2219e9016b35a4fcf2f7d43ebf13d0
Ilya Dyachenko <ilya.dyachenko@maxymiser.com>
2014-09-29 17:31:20 +0300

    backup

---
+++ maxymiser.js
@@ -0,0 +1,35 @@
+/* global mmcore, $ */
+
+(function () {
+    'use strict';
+
+    /**
+     * Set Action snippet (c) Dyachenko
+     * @param actionName
+     * @param actionValue
+     * @param actionAttr
+     * @constructor
+     */
+    function SetAction(actionName, actionValue, actionAttr) {
+        this.actionName = actionName;
+        this.actionValue = actionValue;
+        this.actionAttr = actionAttr || false;
+    }
+
+    SetAction.prototype.deffered = function () {
+        mmcore.$Action(this.actionName, this.actionValue, this.actionAttr);
+    };
+    SetAction.prototype.push = function () {
+        mmcore.SetAction(this.actionName, this.actionValue, this.actionAttr);
+        mmcore._async = true;
+        mmcore.SetPageID('event');
+        mmcore.CGRequest();
+    };
+
+    /* Track Action */
+    new SetAction('T14_Clickthrough', 1).push();
+
+    /* Track Deffered Action */
+    new SetAction('T14_Edit_Cover', 1, 'attr').deffered();
+
+})();
